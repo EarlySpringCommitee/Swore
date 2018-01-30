908ba56aec0e5a71be515e5f1f50e54f51d8f4c7
Rex <rexx0520@gmail.com>
2018-01-30 21:21:05 +0800

    API 回傳資料規範更改

--- js/JsonToTable.js
+++ js/JsonToTable.js
@@ -1,16 +1,17 @@
-function createScoreTable(mode, data, examSelections = Object.keys(data), subjectSelections = Object.keys(Object.values(data)[0]), scoreSelections = '1110') {
+function createScoreTable(mode, data, examSelections = Object.keys(data), subjectSelections = Object.keys(Object.values(data)[0]), scoreSelections = '11111') {
     function s() {
         let table = document.createElement("tbody");
         let firstRow = table.appendChild(document.createElement('tr'));
         firstRow.appendChild(document.createElement('td'));
-        scoreSelections = scoreSelections.split('').concat(['0', '0', '0', '0']);
+        scoreSelections = scoreSelections.split('').concat(['0', '0', '0', '0', '0']);
         let rowScoreLength = scoreSelections.filter(i => i != '0').length;
 
         function generateFirstRow(exam) {
             if (scoreSelections[0] != '0') firstRow.appendChild(document.createElement('td')).appendChild(document.createTextNode(exam));
             if (scoreSelections[1] != '0') firstRow.appendChild(document.createElement('td')).appendChild(document.createTextNode("平均"));
             if (scoreSelections[2] != '0') firstRow.appendChild(document.createElement('td')).appendChild(document.createTextNode("排名"));
-            if (scoreSelections[3] != '0') firstRow.appendChild(document.createElement('td')).appendChild(document.createTextNode("排名人數"));
+            if (scoreSelections[3] != '0') firstRow.appendChild(document.createElement('td')).appendChild(document.createTextNode("類組排"));
+            if (scoreSelections[4] != '0') firstRow.appendChild(document.createElement('td')).appendChild(document.createTextNode("校排"));
         }
 
         function generateScoreRow(subject) {
