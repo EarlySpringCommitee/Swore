43fa3d032ce51eae71f09788bc804f3572a35f5b
GNEHS <jayuiop@gmail.com>
2018-01-31 13:22:00 +0800

    要用時再拿掉註解

--- js/score.js
+++ js/score.js
@@ -1,10 +1,10 @@
 window.onload = function() {
-    var username = window.sessionStorage["username"],
+    /*var username = window.sessionStorage["username"],
         password = window.sessionStorage["password"],
         school = window.sessionStorage["school"]
     if (!username || !password || !school) {
         returnToLogin()
-    }
+    }*/
     let owo = createScoreTable('s', data['s']);
     $("#score").html(owo)
     fillInfoIn(data['i'])
