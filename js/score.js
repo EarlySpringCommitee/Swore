c28f74ceba687290e9575b0aca2ee2f891a270f4
GNEHS <jayuiop@gmail.com>
2018-01-31 13:18:49 +0800

    新增未登入時的成績頁處理

--- js/score.js
+++ js/score.js
@@ -1,5 +1,13 @@
-$(document).ready(function() {
+window.onload = function() {
+    var username = window.sessionStorage["username"],
+        password = window.sessionStorage["password"],
+        school = window.sessionStorage["school"]
+    if (!username || !password || !school) {
+        returnToLogin()
+    }
     let owo = createScoreTable('s', data['s']);
     $("#score").html(owo)
     fillInfoIn(data['i'])
-});
+}
+
+function returnToLogin() { document.location.href = "login.html" }
